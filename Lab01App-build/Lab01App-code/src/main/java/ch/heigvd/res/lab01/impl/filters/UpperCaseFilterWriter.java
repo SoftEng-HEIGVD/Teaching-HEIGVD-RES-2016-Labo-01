b7fcf63792631ce72a0b3cc86277a4b472401a5a
Yves Athanasiadès <yves.athanasiades@heig-vd.ch>
2016-03-09 18:15:23 +0100

    Completed the functions needed to fit the tests. All ok

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -5,28 +5,35 @@ import java.io.IOException;
 import java.io.Writer;
 
 /**
- *
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public UpperCaseFilterWriter(Writer wrappedWriter) {
+        super(wrappedWriter);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        super.write(str.toUpperCase(), off, len);
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+        //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        String str = new String(cbuf);
+        super.write(str.toUpperCase(), off, len);
+
+        //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        Character a = Character.toChars(c)[0];
+        super.write(a.toString().toUpperCase());
+
+        //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    }
 
 }
