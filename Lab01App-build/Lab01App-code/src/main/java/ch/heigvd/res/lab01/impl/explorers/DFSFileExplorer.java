fe54246bec72aa6e3337bae3c8edfc1902c1bd44
Loic Serafin <loicserafin@gmail.com>
2016-03-09 15:36:10 +0100

    Labo 1-fix #3: Application tests. Had to change the DFS explorer, giving files before subdirectories in a given directory.

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -17,12 +17,22 @@ public class DFSFileExplorer implements IFileExplorer {
   @Override
   public void explore(File rootDirectory, IFileVisitor visitor) {
     File[] files;
+    visitor.visit(rootDirectory);
     if (rootDirectory.isDirectory()) {
       files = rootDirectory.listFiles();
-      for (File f: files) //explore recursively all files within the directory
-        explore(f,visitor);
-    } else
-      visitor.visit(rootDirectory); //visit the file using the IFileVisitor
+      for (File f: files) // Explore all the files first
+        if(f.isFile())
+          explore(f,visitor);
+      for (File f: files) // Explore all the subdirectories then
+        if(f.isDirectory())
+          explore(f,visitor);
+    }
+    /*
+     To match the theApplicationShouldBeAbleToGenerateTheListOfFileNames test, we have to guarantee that files
+     are visited before the subdirectories in a given directory. We therefore need to separate the listFiles() in two
+     groups: files and directories, which is done with the two for loops.
+      */
+
 
   }
 
