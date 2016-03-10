775d4e249ab1784bb1552b3a478571bebcd0d276
AkessonHenrik <henrik.akesson@heig-vd.ch>
2016-03-10 18:12:33 +0100

    All tests pass until UpperCaseFilterWriter
    So far the test classes that pass are:
    - ApplicationTest
    - UtilsTest
    - DFSFileExplorerTest
    - FileNumberingFilterWriterTest

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,7 @@
 package ch.heigvd.res.lab01.impl.filters;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -18,24 +20,84 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
     private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
-
+    private int lineNumber = 1;
+    private Boolean waitingForN = false;
     public FileNumberingFilterWriter(Writer out) {
         super(out);
+        try {
+            super.write(Character.forDigit(lineNumber, 10));
+            super.write('\t');
+            lineNumber++;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void write(String str, int off, int len) throws IOException {
-        throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+        if(off < len -1) {
+            for (int i = off; i < off + len; i++) {
+                char c = str.charAt(i);
+                if (c == '\n' || c == '\r') {
+                    if (c == '\r') {
+                        super.write('\r');
+                        if (str.charAt(i + 1) == '\n') {
+                            super.write('\n');
+                            i++;
+                        }
+                    } else {
+                        super.write('\n');
+                    }
+                    super.write(Character.forDigit(lineNumber, 10));
+                    lineNumber++;
+                    super.write('\t');
+                } else {
+                    super.write(c);
+                }
+
+            }
+        }
     }
 
     @Override
     public void write(char[] cbuf, int off, int len) throws IOException {
-        throw new UnsupportedOperationException("The student has not implemented this method yet.");
+//        for(int i = off; i < len; i++) {
+//            if(cbuf[i] == '\r')
+//                if(i < len && cbuf[i+1] == '\n')
+//                    if ( i > off)
+//                        write(cbuf.)
+//        }
     }
 
     @Override
     public void write(int c) throws IOException {
-        throw new UnsupportedOperationException("The student has not implemented this method yet.");
+        // "This is line 1\r\nThis is line 2\nThis is line 3"
+        if(waitingForN) {
+            if((char) c == '\n') {
+                super.write('\r');
+                super.write('\n');
+                super.write(Character.forDigit(lineNumber, 10));
+                lineNumber++;
+                super.write('\t');
+            } else {
+                super.write(c);
+            }
+            waitingForN = false;
+            return;
+        }
+        if((char) c == '\r')
+            waitingForN = true;
+        if(!waitingForN) {
+            if((char) c == '\n') {
+                super.write(c);
+                super.write(Character.forDigit(lineNumber, 10));
+                lineNumber++;
+                super.write('\t');
+            } else {
+                super.write(c);
+            }
+        }
     }
 
 }
