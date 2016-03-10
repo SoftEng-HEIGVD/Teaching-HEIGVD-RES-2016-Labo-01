86f6e28b533ac7bf15bd92e8594e655287ddb26d
Fabien Franchini <fabien.franchini@protonmail.ch>
2016-03-10 22:58:03 +0100

    Implantation de la classe UpperCaseFilterWriter (BUG)

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -16,20 +16,17 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    super.write(str.substring(off, off + len).toUpperCase());
+      super.write(str.substring(off, off + len).toUpperCase());
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    write(new String(cbuf), off, len);
+      write(new String(cbuf), off, len);
   }
 
   @Override
-  public void write(int c) throws IOException { //DRAFT
-      byte[] ch = new byte[1];
-      ch[0] = (byte) c;
-      
-      write(new String(ch, 0, 1, "utf-8"), 0, 1);
+  public void write(int c) throws IOException {
+      super.write(Character.toUpperCase(c));
   }
 
 }
