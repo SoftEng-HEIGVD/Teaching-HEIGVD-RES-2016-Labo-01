eda32cbae51efadf66a09af0173e698e90a237ea
Antoine Drabble <antoine.drabble@gmail.com>
2016-03-10 11:29:38 +0100

    Pass the FileNumberingFilterWriterTest by implementing the 3 methods inside the class

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -22,6 +22,8 @@ public class FileNumberingFilterWriter extends FilterWriter {
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
   
   private int lineNumber = 1;
+  
+  private boolean foundrreturn = false;
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
@@ -30,25 +32,57 @@ public class FileNumberingFilterWriter extends FilterWriter {
   @Override
   public void write(String str, int off, int len) throws IOException {
     String[] lines = Utils.getNextLine(str.substring(off, off + len));
-    for(String line : lines){
-        out.write(lineNumber + "\t" + line);
+    // For the first write we always put the line number
+    if(lineNumber == 1){
+        out.write("1\t");
         lineNumber++;
     }
+    while(!lines[0].isEmpty()){
+        out.write(lines[0]);
+        out.write(lineNumber++ + "\t");
+        lines = Utils.getNextLine(lines[1]);
+    }
+    out.write(lines[1]);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
     String[] lines = Utils.getNextLine(Arrays.toString(cbuf).substring(off, off + len));
-    for(String line : lines){
-        out.write(lineNumber + "\t" + line);
+    if(lineNumber == 1){
+        out.write("1\t");
         lineNumber++;
     }
+    while(!lines[0].isEmpty()){
+        out.write(lines[0]);
+        out.write(lineNumber++ + "\t");
+        lines = Utils.getNextLine(lines[1]);
+    }
+    out.write(lines[1]);
   }
 
-  @Override
-  public void write(int c) throws IOException {
-    out.write(lineNumber + "\t" + c);
-    lineNumber++;
-  }
+    @Override
+    public void write(int c) throws IOException {
+        // Always write the first line number
+        if(lineNumber == 1){
+            out.write("1\t");
+            lineNumber++;
+        }
+        String[] lines = Utils.getNextLine(String.valueOf((char)c));
+        // If there was a \r and next char is not a \n, add a line return
+        if(foundrreturn && (char)c != '\n')
+            out.write(lineNumber++ + "\t");
+        foundrreturn = false;
+        // If there is a \r wait for next char to see if it is a \n
+        if((char)c == '\r'){
+            out.write('\r');
+            foundrreturn = true;
+        }
+        // If the char is not a \r write it
+        else{
+            if(!lines[0].isEmpty())
+                out.write(lines[0] + lineNumber++ + "\t");
+            out.write(lines[1]);
+        }
+    }
 
 }
