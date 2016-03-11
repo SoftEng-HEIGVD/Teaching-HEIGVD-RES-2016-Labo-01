40f0854cb97ef4605906076d8786ec0681e28e75
Sébastien Richoz <sebrichoz@gmail.com>
2016-03-11 16:26:51 +0000

    Implementation of Utils.java

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
@@ -21,22 +21,53 @@ public class Utils {
    */
   public static String[] getNextLine(String lines) {
       
-      // lineSeparator is dependant from the OS, so the tests
-      // for Unix and Mac won't pass...
-      // TODO
-      int indexOflineSep = lines.indexOf(System.lineSeparator());
-      int sepLength = System.lineSeparator().length();
+      int[] sepInfos = getSeparatorPosition(lines);
+      int posSeparator = sepInfos[0];
+      int lenSeparator = sepInfos[1];
       
       String arg1, arg2;
-      if(indexOflineSep == -1) {
+      if(posSeparator == -1) {
           arg1 = "";
           arg2 = lines;
       } else {
-          arg1 = lines.substring(0, indexOflineSep + sepLength);
-          arg2 = lines.substring(indexOflineSep + sepLength, lines.length());
+          arg1 = lines.substring(0, posSeparator + lenSeparator);
+          arg2 = lines.substring(posSeparator + lenSeparator, lines.length());
       }
       
       return new String[] {arg1, arg2};
   }
-
+  
+  /**
+   * Find the first occurence of \r, \n, or \r\n in a string and return
+   * its position in that string and its length.
+   * 
+   * @param s is the String to retrieve from the first separator
+   * @return the first element is the position and the second is the length
+   * of the separator
+   */
+  private static int[] getSeparatorPosition(String s) {
+      int pos = -1, len = -1, sLen = s.length();
+      char[] sArray = s.toCharArray();
+      
+      for(int i = 0 ; i < sLen ; ++i) {
+          
+          if(sArray[i] == '\r') {
+              pos = i;
+              if(i+1 < sLen && sArray[i+1] == '\n') {
+                  len = 2;
+                  break;
+              } else {
+                  len = 1;
+                  break;
+              }
+          } else if(sArray[i] == '\n') {
+              pos = i;
+              len = 1;
+              break;
+          }
+              
+      }
+      
+      return new int[] {pos, len};
+  }
 }
