7eaa09183adbaac0e8d634b83420dc7b95c7c59f
Sébastien Richoz <sebrichoz@gmail.com>
2016-03-11 06:40:37 +0000

    DFS implementation

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Stack;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -14,9 +15,30 @@ import java.io.File;
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    @Override
+    public void explore(File rootDirectory, IFileVisitor visitor) {
+        // Use a stack to store directories for DFS
+        Stack<File> stack = new Stack<>();
+        stack.push(rootDirectory);
+
+        while(!stack.empty()) {
+            File dir = stack.pop();
+            
+            visitor.visit(dir);
+            
+            // List all files of the element poped from the stack
+            // and visit them if they exist.
+            File[] fileAndDirList = dir.listFiles();
+            if(fileAndDirList != null) {
+                // 
+                for(int i = fileAndDirList.length-1 ; i >= 0 ; i--) {
+                    if(fileAndDirList[i].isDirectory())
+                        stack.push(fileAndDirList[i]);
+
+                    visitor.visit(fileAndDirList[i]);
+                }
+            }
+        }
   }
 
 }
