50cde5845b072d95b2c240ad46a100125b7fa073
Mathieu Urstein <mathieu.urstein@gmail.com>
2016-03-11 10:06:05 +0100

    27 passed tests

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,20 +3,37 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.LinkedList;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
  * exploration of the file system and invokes the visitor for every encountered
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
- * 
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
-
+    @Override
+    public void explore(File position, IFileVisitor visitor) {
+        // visit
+        visitor.visit(position);
+        // get all subdirectories and files
+        final File[] files = position.listFiles();
+        // if there is files or directories
+        if (files != null) {
+            // for every files in this directory
+            for (File child : files) {
+                if (child.isFile())
+                    visitor.visit(child);
+            }
+            // for every subdirectories
+            for(File child : files) {
+                if (child.isDirectory())
+                    explore(child, visitor);
+            }
+        }
+    }
 }
+
--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -5,28 +5,35 @@ import java.io.IOException;
 import java.io.Writer;
 
 /**
+ * Makes every string uppercase
  *
- * @author Olivier Liechti
+ * @author Mathieu Urstein
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public UpperCaseFilterWriter(Writer wrappedWriter) {
+        super(wrappedWriter);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        out.write(str.substring(off, off+len).toUpperCase());
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        // for every requested char
+        for (int i = 0; i < len; i++) {
+            // make it uppercase
+            cbuf[off+i] = Character.toUpperCase(cbuf[off+i]);
+            out.write(cbuf[off+i]);
+        }
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        // just write uppercase version of c
+        out.write(Character.toUpperCase(c));
+    }
 
 }
