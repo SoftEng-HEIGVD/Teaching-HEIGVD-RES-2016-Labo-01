53814b57af65af0eaefb4964f111dbef19eeb8a0
Benjamin Schubert <ben.c.schubert@gmail.com>
2016-03-11 22:17:02 +0100

    implemented FileNumbering writer

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -1,15 +1,9 @@
 package ch.heigvd.res.lab01.impl.filters;
 
-import com.sun.xml.internal.ws.api.WSService;
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.logging.Logger;
-
-import static ch.heigvd.res.lab01.impl.Utils.getNextLine;
-import static java.lang.Integer.min;
 
 /**
  * This class transforms the streams of character sent to the decorated writer.
@@ -25,46 +19,81 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
   private int counter = 1;
+  private boolean eol = true;
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
+  /**
+   * writes a string to the outputstream.
+   *
+   * For the sake of simplicity and DRY principle, we just convert this
+   * to an array and call the write with a bytearray
+   *
+   * @param str: the string to write
+   * @param off: offset at which to start writing
+   * @param len: length to write
+   */
   @Override
   public void write(String str, int off, int len) throws IOException {
-
-    String[] nextLine = getNextLine(str);
-    while(0 != nextLine[0].length()) {
-      try {
-        super.write(nextLine[1], off, len);
-      }
-      catch(IndexOutOfBoundsException ignored) {}
-
-      len -= nextLine[0].length();
-      off = min(nextLine[0].length(), 0);
-
-      if(len < 0) {
-        return;
-      }
-
-      super.write(counter);
-      super.write('\t');
-      nextLine = getNextLine(nextLine[1]);
-    }
-
-    if(0 != nextLine[1].length()) {
-      super.write(nextLine[1], off, len);
-    }
+    write(str.toCharArray(), off, len);
   }
 
+  /**
+   * Writes a bytearray to the outputstream.
+   * This function internally calls write on a byte by byte way,
+   * It should not impact performance seriously if you use
+   * a buffered outputstream, which is recommended
+   *
+   * @param cbuf: buffer to write
+   * @param off: offset at which to start writing
+   * @param len: length to write
+   * @throws IOException
+     */
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    super.write(cbuf, off, len);
+    for(int i=off; i < (len + off); i++) {
+      write(cbuf[i]);
+    }
   }
 
+  /**
+   * Writes the number of the line and a tab
+   *
+   * @throws IOException
+     */
+  private void write_line_header() throws IOException {
+    eol = false;
+
+    for(char x: Integer.toString(counter).toCharArray()) {
+      super.write(x);
+    }
+    counter++;
+    super.write('\t');
+  }
+
+  /**
+   * writes the int (byte) given in parameter to the stream.
+   * If this is a new line, also adds the line header afterwards
+   *
+   * @param c: value to write
+   * @throws IOException
+     */
   @Override
   public void write(int c) throws IOException {
+    if(eol && ('\n' != c)) {
+      write_line_header();
+    }
+
     super.write(c);
+
+    if('\r' == c) {
+      eol = true;
+    }
+    else if('\n' == c) {
+      write_line_header();
+    }
   }
 
 }
