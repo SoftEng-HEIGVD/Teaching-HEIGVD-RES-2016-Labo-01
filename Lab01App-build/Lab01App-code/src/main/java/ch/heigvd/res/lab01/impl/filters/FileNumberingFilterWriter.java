54240c1814a25ae85f24f4ec7ab06e43836634f3
Basile Vu <basile.vu@gmail.com>
2016-03-12 20:30:14 +0100

    FileNumbering works except int

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -21,6 +21,7 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
   private int lineNum = 0;
+  private boolean wroteNewLine;
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
@@ -28,8 +29,8 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    str = transform(str);
-    out.write(str, off, str.length());
+    str = transform(str.substring(off, off+len));
+    out.write(str, 0, str.length());
   }
 
   @Override
@@ -44,24 +45,35 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   private String transform(String s) {
 
-    String[] tmp = Utils.getNextLine(s);
-    String res = lineNum == 0 ? (++lineNum) + "\t" + s : s;
-
     /*
-     * After Utils.getNextLine:
-     * - tmp[0] == "" -> no line separator in the line passed to getNextLine
-     * - tmp[1] == "" -> line passed is the last line and finishes with a line separator
-     * So if there is a line separator, we must write next line with the number and a \t
+     * See getNextLine for detailed behaviour. The important facts in this function are :
+     * - tmp[0] == "" -> no line separator in the line passed to getNextLine.
+     * - tmp[1] == "" -> line passed is the last line and finishes with a line separator.
      */
+    String[] tmp = Utils.getNextLine(s);
+
+    // If it's the first line, we add the line number and a tab.
+    String res = lineNum == 0 ? (++lineNum) + "\t" + tmp[0] : tmp[0];
+
+    // We can return res here if s does not contain any line separator.
+    if (tmp[0].equals("")) {
+      res += tmp[1];
+      return res;
+    }
+
+    // While there are still a line to transform -> line number + tab char + line
     while (!tmp[0].equals("")) {
       tmp = Utils.getNextLine(tmp[1]);
-      res += (++lineNum) + "\t" + tmp[0];
+      // We write the line. If it's the last, we must take that into account too and write tmp[1] instead
+      res += (++lineNum) + "\t" + (tmp[0].equals("") ? tmp[1] : tmp[0]);
 
       // The new tmp[1] is "" ->  the new tmp[0] is the last line and finishes with a line separator.
+      // Since the result of getNextLine will be the same next iteration, we can quit here.
       if (tmp[1].equals("")) {
         break;
       }
     }
+
     return res;
   }
 }
