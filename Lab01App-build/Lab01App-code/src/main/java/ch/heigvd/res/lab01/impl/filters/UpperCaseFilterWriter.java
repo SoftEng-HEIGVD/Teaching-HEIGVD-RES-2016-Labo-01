6b50a15bd661d5e9ca3da053392853c00e840e22
Amel Dussier <amel.dussier@gmail.com>
2016-03-12 11:41:44 +0100

    Everything but the "theApplicationShouldBeAbleToGenerateTheListOfFileNames" test passes

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -9,24 +9,28 @@ import java.io.Writer;
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public UpperCaseFilterWriter(Writer wrappedWriter) {
+        super(wrappedWriter);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        // write uppercase string
+        super.write(str.toUpperCase(), off, len);
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        // uppercase the needed chars of the char array
+        for (int i = off; i < off + len; i++)
+            cbuf[i] = Character.toUpperCase(cbuf[i]);
+        super.write(cbuf, off, len);
+    }
 
+    @Override
+    public void write(int c) throws IOException {
+        // write uppercase char
+        super.write(Character.toUpperCase(c));
+    }
 }
