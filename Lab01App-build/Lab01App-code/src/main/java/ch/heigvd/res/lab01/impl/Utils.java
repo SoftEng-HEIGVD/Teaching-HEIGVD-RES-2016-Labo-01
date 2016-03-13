2d0afd6330ad06f7f2300b135012719cbc2373e6
Guillaume Serneels <guillaume.serneels@heig-vd.ch>
2016-03-13 17:03:53 +0100

    UtilsTest done

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
@@ -19,8 +19,59 @@ public class Utils {
    * the line separator, the second element is the remaining text. If the argument does not
    * contain any line separator, then the first element is an empty string.
    */
-  public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public static String[] getNextLine(String lines) {
+        
+        String[] linesArray = new String[2];
+        int index = 0;
+        boolean newLineOccurence = false;
+        
+        if(lines.contains("\r\n")){
+            index=lines.indexOf("\r\n") + 1;
+            newLineOccurence = true;
+        }else if(lines.contains("\r")){
+            index=lines.indexOf("\r");
+            newLineOccurence = true;
+        }else if(lines.contains("\n")){
+            index= lines.indexOf("\n");
+            newLineOccurence = true;
+        }
+        if(newLineOccurence){
+            linesArray[0] = lines.substring(0, index + 1);
+            linesArray[1] = lines.substring(index + 1);
+        }else{
+            linesArray[0] = "";
+            linesArray[1] = lines;            
+        }  
+        return linesArray;
+    }
+}                
+        /*
+        
+        for (int i = 0; i < lines.length(); i++) {
+            char c = lines.charAt(i);
+            
+            if (c == '\n' || c == '\r') {
 
-}
+                if (c == '\r' && i != (lines.length())) {
+                    if (lines.charAt(i + 1) == '\n') {
+                        i++;
+                        linesArray[0] = lines.substring(0, i + 1);
+                        linesArray[1] = lines.substring(i + 1);
+                        return linesArray;
+                    }
+                }
+                linesArray[0] = lines.substring(0, i + 1);
+                linesArray[1] = lines.substring(i + 1);
+                return linesArray;
+            }
+        }
+        linesArray[0] = "";
+        linesArray[1] = lines;
+        return linesArray;
+    }   
+}  */  //Process char
+
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+  
+
+
