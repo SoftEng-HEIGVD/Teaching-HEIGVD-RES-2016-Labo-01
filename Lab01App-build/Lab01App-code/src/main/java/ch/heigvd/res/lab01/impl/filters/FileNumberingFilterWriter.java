06c928af2a8569478e1246e7ee4ead13da81e63a
galedric <g@ledric.me>
2016-03-14 15:00:57 +0100

    Implement FileNumberingFilterWriter

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -10,32 +10,50 @@ import java.util.logging.Logger;
  * When filter encounters a line separator, it sends it to the decorated writer.
  * It then sends the line number and a tab character, before resuming the write
  * process.
- *
+ * <p>
  * Hello\n\World -> 1\Hello\n2\tWorld
  *
  * @author Olivier Liechti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+	private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+	private int line = 1;
+	private boolean eol = true;
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+	public FileNumberingFilterWriter(Writer out) {
+		super(out);
+	}
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+	@Override
+	public void write(String str, int off, int len) throws IOException {
+		write(str.toCharArray(), off, len);
+	}
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+	@Override
+	public void write(char[] cbuf, int off, int len) throws IOException {
+		for (int i = off; i < off + len; ++i) {
+			write(cbuf[i]);
+		}
+	}
+
+	private void writeLineNumber() throws IOException {
+		out.write(String.format("%d\t", line++));
+		eol = false;
+	}
+
+	@Override
+	public void write(int c) throws IOException {
+		if (eol && c != '\n') writeLineNumber();
+
+		out.write(c);
+
+		if (c == '\r') {
+			eol = true;
+		} else if (c == '\n') {
+			writeLineNumber();
+		}
+	}
 
 }
