b8a4b6847ed4247f763a484890ca394c26f9819b
Ciani Antony <antony.ciani@heig-vd.ch>
2016-03-15 20:44:49 +0100

    Labo01_AlmostFinished

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,20 +3,56 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.io.FileFilter;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
- * exploration of the file system and invokes the visitor for every encountered
- * node (file and directory). When the explorer reaches a directory, it visits all
- * files in the directory and then moves into the subdirectories.
- * 
+ * exploration of the file system and invokes the visitor for every encountered node
+ * (file and directory). When the explorer reaches a directory, it visits all files
+ * in the directory and then moves into the subdirectories.
+ *
  * @author Olivier Liechti
+ * @author Ciani Antony
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    private FileFilter directoryFilter = new FileFilter() {
+                @Override
+                public boolean accept(File pathname) {
+                    if (pathname.isDirectory()) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            };
+    private FileFilter fileFilter = new FileFilter() {
+                @Override
+                public boolean accept(File pathname) {
+                    if (pathname.isFile()) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            };
+
+    @Override
+    public void explore(File rootDirectory, IFileVisitor vistor) {
+
+        vistor.visit(rootDirectory);
+        if (rootDirectory.isDirectory()) {
+            File[] filesInDirectory = rootDirectory.listFiles(this.fileFilter);
+            File[] subdirectories = rootDirectory.listFiles(this.directoryFilter);
+
+            for (File f : filesInDirectory) {
+                vistor.visit(f);
+            }
+            for (File f : subdirectories){
+                explore(f, vistor);
+            }
+        }
+
+    }
 
 }
