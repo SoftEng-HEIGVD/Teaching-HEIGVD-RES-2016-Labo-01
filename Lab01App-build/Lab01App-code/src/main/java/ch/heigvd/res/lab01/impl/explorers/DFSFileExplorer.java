f59d3c44249631bf73033d17b842e43381fd133c
vagrant <vagrant@RES>
2016-03-16 20:36:06 +0000

    Labo01

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,20 +3,39 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.LinkedList;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
- * exploration of the file system and invokes the visitor for every encountered
- * node (file and directory). When the explorer reaches a directory, it visits all
- * files in the directory and then moves into the subdirectories.
- * 
+ * exploration of the file system and invokes the visitor for every encountered node
+ * (file and directory). When the explorer reaches a directory, it visits all files
+ * in the directory and then moves into the subdirectories.
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+   @Override
+   public void explore(File rootDirectory, IFileVisitor vistor) {
+      //visit the directory in argument and test whether it has file or subdirecty
+      LinkedList<File> directories = new LinkedList<File>();
+      vistor.visit(rootDirectory);
+      if (rootDirectory.listFiles() == null) // if there is no file in the rootDirectory we did not do anything
+      {
+         return;
+      }
+      for (File f : rootDirectory.listFiles()) { // We visit all the file in the rootDirectory
+         if (f.isDirectory()) // if one of the files is also adirectory, we explore it
+         {
+            directories.add(f);
+         } else {
+            vistor.visit(f);// else we visit the file
+         }
+      }
 
+      for (File f : directories) {
+         explore(f, vistor);
+      }
+   }
+  
 }
--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -1,15 +1,17 @@
 package ch.heigvd.res.lab01.impl.filters;
 
+import ch.heigvd.res.lab01.impl.Utils;
+import java.io.File;
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.LinkedList;
 import java.util.logging.Logger;
 
 /**
- * This class transforms the streams of character sent to the decorated writer.
- * When filter encounters a line separator, it sends it to the decorated writer.
- * It then sends the line number and a tab character, before resuming the write
- * process.
+ * This class transforms the streams of character sent to the decorated writer. When
+ * filter encounters a line separator, it sends it to the decorated writer. It then
+ * sends the line number and a tab character, before resuming the write process.
  *
  * Hello\n\World -> 1\Hello\n2\tWorld
  *
@@ -17,25 +19,49 @@ import java.util.logging.Logger;
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  
+   private char charcterBefore = '\0';
+   private int lineNumber  = 0;
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+   public FileNumberingFilterWriter(Writer out) {
+      super(out);
+   }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+   @Override
+   public void write(String str, int off, int len) throws IOException {
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+      String cbuf = str.substring(off, off + len);
+      for (int i = off; i < off + len; i++) {
+         this.write(str.charAt(i));
+      }
+   }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+   @Override
+   public void write(char[] cbuf, int off, int len) throws IOException {
+      this.write(new String(cbuf), off, len);
+   }
 
+   @Override
+   public void write(int c) throws IOException {
+
+      if (lineNumber  == 0) {
+         out.write(++lineNumber  + "\t");
+      }
+
+      if ((char) c == '\n') {
+         out.write((char) c);
+         out.write(++lineNumber  + "\t");
+         charcterBefore = (char) c;
+      } else {
+         if ( charcterBefore == '\r') { 
+            out.write(++lineNumber  + "\t");            
+         }
+         out.write((char) c);
+         charcterBefore = (char) c;
+      }
+   
+   }
+
+      
 }
