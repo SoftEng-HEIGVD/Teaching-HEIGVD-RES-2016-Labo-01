6e2f306d76731d2aea8cdbd2c8c4c193a6f49f71
sandra <annie.dongmongoumnai@heig-vd.ch>
2016-03-16 21:25:50 +0100

    Labo01-def

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,6 @@
 package ch.heigvd.res.lab01.impl.filters;
 
+import ch.heigvd.res.lab01.impl.Utils;
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -18,24 +19,58 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
-
+  private int lineNumber =0;
+  private char previousCharacter ='\0';
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+     for(int i=off; i < off+len;++i){
+       this.write(str.charAt(i));
+     }
+    
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    this.write(new String(cbuf), off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+     /*if it is the first line we write the number*/
+     if(lineNumber==0){
+        writeLineNumber();
+     }
+     /*if is the end of line*/
+     if(c == '\n'){
+        super.write('\n');
+        /*if the character was not \r\n(end of line)*/
+        if(previousCharacter != '\r'){
+           writeLineNumber();  
+        }
+        /*if its \r\n */
+        else{ 
+           writeLineNumber();
+        }
+     }
+     else {
+        if(previousCharacter == '\r')
+           writeLineNumber();
+        super.write(c);
+     }
+     
+     previousCharacter = (char) c;
+  }
+  /*this method convert the line number into string and write the it with the '\t'*/
+  private void writeLineNumber() throws IOException{
+     
+      String lineNumberStr = Integer.toString(++lineNumber);
+           for(int i =0; i< lineNumberStr.length();++i)
+              super.write(lineNumberStr.charAt(i));
+           super.write('\t');
   }
 
 }
--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -16,17 +16,20 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+     super.write(str.toUpperCase(), off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+     /*we convert the tab of char into string and we call  our method write */
+     String str = new String(cbuf);
+     this.write(str, off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+     /*we convert the character to UpperCase and we call write of the heritate class*/
+    super.write(Character.toUpperCase(c));
   }
 
 }
