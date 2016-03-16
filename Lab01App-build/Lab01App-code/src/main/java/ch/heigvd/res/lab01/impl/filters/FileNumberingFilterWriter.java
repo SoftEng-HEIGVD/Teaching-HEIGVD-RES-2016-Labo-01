7f0802d52bdb2d70a6fc9453fb51438330476b19
Fabien Franchini <fabien.franchini@protonmail.ch>
2016-03-16 12:41:03 +0100

    Bugfix, on write(int)

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -4,6 +4,8 @@ import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.util.logging.Logger;
+import ch.heigvd.res.lab01.impl.Utils;
+import java.util.logging.Level;
 
 /**
  * This class transforms the streams of character sent to the decorated writer.
@@ -11,31 +13,75 @@ import java.util.logging.Logger;
  * It then sends the line number and a tab character, before resuming the write
  * process.
  *
- * Hello\n\World -> 1\Hello\n2\tWorld
+ * Hello\n\World -> 1\tHello\n2\tWorld
  *
  * @author Olivier Liechti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private int currentLineNumber;
+  private int previousChar;
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
+    currentLineNumber = 1;
+      try {
+          out.write(currentLineNumber + "\t");
+      } catch (IOException ex) {
+          Logger.getLogger(FileNumberingFilterWriter.class.getName()).log(Level.SEVERE, null, ex);
+      }
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      String[] lines = new String[2];
+      String result = "";
+      
+      // Ce placer au bon offset
+      lines[1] = str.substring(off, off + len);
+      
+      do {
+          lines = Utils.getNextLine(lines[1]);
+          
+          if (lines[0].equals("")) {
+              result += lines[1];
+              break;
+          }
+          else {
+              result += lines[0];
+              
+              if (lines[0].endsWith("\n") || lines[0].endsWith("\r")) {
+                  currentLineNumber++;
+                  result += currentLineNumber + "\t";
+              }           
+          }
+      } while(!lines[0].equals(""));
+
+      super.write(result, 0, result.length());
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      write(new String(cbuf), off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      if (c == '\r') {
+          previousChar = '\r';
+          return;
+      }
+      else if (c == '\n' && previousChar == '\r') {
+          previousChar = c;
+          write("\r\n", 0, 2);
+          return;
+      }
+      else if (c != '\n' && previousChar == '\r') {
+          write(new String(Character.toChars('\r')), 0, 1);
+      }
+      
+      write(new String(Character.toChars(c)), 0, 1);
   }
 
 }
