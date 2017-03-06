7d711bc4b13600cbc79c3d98bcddf8129e1bdfb2
Christopher Meier <christopher.meier@heig-vd.ch>
2017-03-06 21:34:10 +0100

    Working DFSFileExplorer.java

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,9 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -16,7 +19,26 @@ public class DFSFileExplorer implements IFileExplorer {
 
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    vistor.visit(rootDirectory);
+
+    if(rootDirectory.isDirectory()){
+      File[] subFiles = rootDirectory.listFiles();
+      ArrayList<File> subDir = new ArrayList<>();
+
+      Arrays.sort(subFiles);
+
+      for(File f : subFiles) {
+        if(f.isDirectory()) {
+          subDir.add(f);
+        } else {
+          explore(f, vistor);
+        }
+      }
+
+      for(File f : subDir) {
+        explore(f, vistor);
+      }
+    }
   }
 
 }
