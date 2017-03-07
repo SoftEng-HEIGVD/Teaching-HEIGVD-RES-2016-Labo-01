aecbae831e8db11e9f6de0933d17412532351bb5
Newtt <m.chatelan@gmail.com>
2017-03-07 12:27:51 +0100

    filenumberwithfilter working

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -19,14 +19,16 @@ public class FileNumberingFilterWriter extends FilterWriter
 {
 
    private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
-   private static int counter = 1;
-   private static boolean firstCall = true;
+   private  int counter;
+   private  boolean firstCall;
+   private boolean previousWasCR;
 
    public FileNumberingFilterWriter(Writer out)
    {
       super(out);
       counter = 1;
       firstCall = true;
+      previousWasCR = false;
    }
 
    @Override
@@ -36,35 +38,12 @@ public class FileNumberingFilterWriter extends FilterWriter
       if (str == null)
          return;
 
-      String result;
+      // Make a substring as required and turn it into a char[]
+      String string = str.substring(off, off + len);
+      char[] cbuf = string.toCharArray();
 
-      // If the string contain a \n or a \r in the middle, jump to the appropriate write function
-      /*if (str.indexOf('\n') != str.length() - 1 || str.indexOf('\r') != str.length()-1)
-      {
-         write(str.toCharArray(), off, len);
-         return;
-      }*/
-
-      // If it's the first call to the method, we have to put the number in front
-      if (firstCall)
-      {
-         result = counter++ + "\t" + str.substring(off, off + len);
-         firstCall = false;
-      }
-      else // Just add the string without modification
-      {
-         result = str.substring(off, off + len);
-      }
-
-      // We add the formatted string to the Writer
-      super.write(result, 0, result.length());
-
-      // If there is a new line at the end, add the next number bellow
-      if (str.contains("\r") || str.contains("\n"))
-      {
-         String nextLine = counter++ + "\t";
-         super.write(nextLine, 0, nextLine.length());
-      }
+      // Send the buffer to the appropriate function
+      write(cbuf, 0, cbuf.length);
    }
 
    @Override
@@ -74,6 +53,7 @@ public class FileNumberingFilterWriter extends FilterWriter
       if (cbuf == null)
          return;
 
+      // Send each individual char to the lower level write function
       for (char c : cbuf)
          write(c);
    }
@@ -81,27 +61,42 @@ public class FileNumberingFilterWriter extends FilterWriter
    @Override
    public void write(int c) throws IOException
    {
-      String result;
-
-      // If it's the first call to the method, add the line number at the beginning
-      if (firstCall)
+      // Beginning of line because it's the first call to the write function
+      if(firstCall)
       {
-         result = counter++ + "\t";
-         super.write(result, 0, result.length());
+         // Add the number and the tab at the beginning
+         String str = counter++ + "\t";
+         super.write(str, 0, str.length());
+
          firstCall = false;
       }
 
-      // If the last char is a return, add it and the new counter
+      String str;
+
+      // If it's a new line with a \n
       if (c == '\n')
       {
-         result = '\n' + counter++ + "\t";
-         super.write(result, 0, result.length());
+         str = "\n" + counter++ + "\t";
+         previousWasCR = false;
+         super.write(str, 0, str.length());
       }
-      // If it's a regular char, add it
+      // Otherwise if we have a \r new line and no previous \r
+      else if (c == '\r' && !previousWasCR)
+      {
+         str = "\r";
+         previousWasCR = true;
+         super.write(str, 0, str.length());
+      }
       else
       {
-         // Write the char to the output
+         // If the previous written char was a CR
+         if(previousWasCR)
+         {
+            str = counter++ + "\t";
+            super.write(str, 0, str.length());
+         }
          super.write(c);
+         previousWasCR = false;
       }
    }
 }
