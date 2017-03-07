133fa81fd95ec13add48d7f9648e9433e0d7a68c
Colin Lavanchy <colin.lavanchy@heig-vd.ch>
2017-03-07 23:12:15 +0100

    FileNumbering decorator passing all tests -> implementation can be improved

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,7 @@
 package ch.heigvd.res.lab01.impl.filters;
 
+import ch.heigvd.res.lab01.impl.Utils;
+
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -17,6 +19,16 @@ import java.util.logging.Logger;
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
+  private int lineCount = 1;
+  private STATE state = STATE.NEW_LINE;
+
+  public enum STATE{
+      NEW_LINE,
+      END_LINE,
+      R_READ,
+      STANDARD
+  }
+
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
   public FileNumberingFilterWriter(Writer out) {
@@ -25,17 +37,42 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+        for(int i=off;i<off+len;i++)
+            write(str.charAt(i));
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      for(int i=off;i<off+len;i++)
+          write(cbuf[i]);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+      //Checking if previous \r was single separator
+      if(state.equals(STATE.R_READ)&&c!='\n'){
+            state = STATE.NEW_LINE;
+          out.write(lineCount++ + "\t");
+      }
+
+      //If new line
+      if (state.equals(STATE.NEW_LINE)) {
+          if(lineCount==1)
+              out.write(lineCount++ + "\t");
+          state = STATE.STANDARD;
+      }
+
+      out.write(c);
+
+      //Check if \r was previously read and \n just written -> \r\n separator
+      //or \n single separator
+      if(c=='\n') {
+          state = STATE.NEW_LINE;
+          out.write(lineCount++ + "\t");
+      }
+      else if(c=='\r')
+          state = STATE.R_READ;
   }
 
 }
