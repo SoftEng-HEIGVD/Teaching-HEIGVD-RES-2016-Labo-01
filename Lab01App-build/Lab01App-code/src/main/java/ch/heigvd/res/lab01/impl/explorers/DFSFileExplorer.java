b331b38dce2697a981d5df7fb7aed152dc442ef6
Daniel Palumbo <dpalumbo06@gmail.com>
2017-03-07 11:58:53 +0100

    DFS all test green

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,8 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -16,7 +18,38 @@ public class DFSFileExplorer implements IFileExplorer {
 
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      if(rootDirectory == null){
+          throw new UnsupportedOperationException("Root directory doesn't exist !");
+      }
+
+      // Visit current node
+      vistor.visit(rootDirectory);
+
+      // check if the file is a directory
+      if(rootDirectory.isDirectory()){
+          // Create table that contains file in the directory
+          File[] files = rootDirectory.listFiles();
+
+          // Create and sort table that contains the list of subdirectory
+          ArrayList<File> subDirectories = new ArrayList<>();
+          Arrays.sort(files);
+
+          for(File f : files){
+              if(f.isDirectory()){
+                  // add directory to the list of subdirectory
+                  subDirectories.add(f);
+              }
+              else{
+                  // recursive call
+                  explore(f, vistor);
+              }
+          }
+
+          // Explore sub directories
+          for(File f : subDirectories){
+              explore(f, vistor);
+          }
+      }
   }
 
 }
