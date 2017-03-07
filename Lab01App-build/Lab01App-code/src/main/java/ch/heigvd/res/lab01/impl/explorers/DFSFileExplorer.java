4c22375869d075444014f07e8d73c560c0673098
Lucas Elisei <faku99dev@gmail.com>
2017-03-07 14:13:05 +0100

    Pass all tests for ApplicationTest

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -5,6 +5,9 @@ import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 
 import java.io.File;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
  * exploration of the file system and invokes the visitor for every encountered
@@ -17,8 +20,27 @@ public class DFSFileExplorer implements IFileExplorer {
 
     @Override
     public void explore(File rootDirectory, IFileVisitor visitor) {
+        // We visit the current directory.
+        visitor.visit(rootDirectory);
+
+        // We store the directories so we can process them later.
+        List<File> directories = new ArrayList<File>();
+
+        // Then, we iterate over all the files in the current directory.
         for(File file : rootDirectory.listFiles()) {
-            visitor.visit(file);
+            // If the file is indeed a file, we let the visitor visit it.
+            if(file.isFile()) {
+                visitor.visit(file);
+            }
+            // If the file is a directory, we add it to the list.
+            else {
+                directories.add(file);
+            }
+        }
+
+        // After processing the files, we process the directories.
+        for(File directory : directories) {
+            this.explore(directory, visitor);
         }
     }
 
