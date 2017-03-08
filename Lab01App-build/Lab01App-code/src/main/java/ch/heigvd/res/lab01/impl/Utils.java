f934089be04adfb0d2a1cf9855511de5457ea045
Tano <tano.iannetta@heig-vd.ch>
2017-03-08 20:11:03 +0000

    passe UtilsTest

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
@@ -2,6 +2,11 @@ package ch.heigvd.res.lab01.impl;
 
 import java.util.logging.Logger;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.lang.String;
+
 /**
  *
  * @author Olivier Liechti
@@ -12,15 +17,47 @@ public class Utils {
 
   /**
    * This method looks for the next new line separators (\r, \n, \r\n) to extract
-   * the next line in the string passed in arguments. 
-   * 
+   * the next line in the string passed in arguments.
+   *
    * @param lines a string that may contain 0, 1 or more lines
    * @return an array with 2 elements; the first element is the next line with
    * the line separator, the second element is the remaining text. If the argument does not
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+      String[] ArrayLines = new String[2];
+
+      int pos = lines.indexOf('\r', 0);
+      if(pos != -1) // \r found
+      {
+          ArrayLines[0] = lines.substring(0, ++pos);
+
+          // windows
+          if(pos < lines.length() && lines.charAt(pos) == '\n')
+          {
+              ArrayLines[0] += '\n';//lines.substring(pos+1, pos+2);
+              pos++;
+          }
+
+      }
+      else   // looking for only \n, unix
+      {
+          pos = lines.indexOf('\n', 0);
+          if(pos != -1) // found
+          {
+              ArrayLines[0] = lines.substring(0, ++pos);
+          }
+          else // no other line
+          {
+             ArrayLines[0] = "";
+             ArrayLines[1] = lines;
+             return ArrayLines;
+          }
+      }
+
+      ArrayLines[1] = lines.substring(pos); // rest of the lines
+      return ArrayLines;
   }
 
 }
