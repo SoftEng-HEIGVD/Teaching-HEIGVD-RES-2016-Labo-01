2dec10338f3811731e9ee9c15b720a19a84eb67c
Naewy <katomey@gmail.com>
2017-03-08 09:46:26 +0100

    UpperCaseFilterWriter.java implemented to pass the 5 tests in UppeCaseFilterWriterTest.java

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -6,27 +6,41 @@ import java.io.Writer;
 
 /**
  *
- * @author Olivier Liechti
+ * @author Olivier Liechti 
+ * Modified by Lara Chauffoureaux on 08.03.2017
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public UpperCaseFilterWriter(Writer wrappedWriter) {       
+        super(wrappedWriter);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+        String upperStr = str.toUpperCase();
 
+        // i use the wrapped writer write methods, thus i can call directly have 
+        // the version that suits me
+        out.write(upperStr, off, len);
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+
+        char[] upperCbuf = new char[cbuf.length];
+
+        for (int i = 0; i < cbuf.length; i++) {
+
+            upperCbuf[i] = Character.toUpperCase(cbuf[i]);
+        }
+
+        out.write(upperCbuf, off, len);
+    }
+    
+    @Override
+    public void write(int c) throws IOException {
+
+        out.write(Character.toUpperCase((char) c));
+    }
 }
