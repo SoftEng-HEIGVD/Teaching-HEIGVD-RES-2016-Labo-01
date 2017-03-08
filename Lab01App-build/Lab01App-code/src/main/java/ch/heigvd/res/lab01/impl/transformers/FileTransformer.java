67be0ccd2613e4880c40660ec823c5d45c84a0f8
Naewy <katomey@gmail.com>
2017-03-08 10:36:34 +0100

    FileTransformer.java and NoOpFileTransformer.java implemented to pass the test in FileTransformerTest.java

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/transformers/FileTransformer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/transformers/FileTransformer.java
@@ -27,44 +27,51 @@ import java.util.logging.Logger;
  */
 public abstract class FileTransformer implements IFileVisitor {
 
-  private static final Logger LOG = Logger.getLogger(FileTransformer.class.getName());
-  private final List<FilterWriter> filters = new ArrayList<>();
+    private static final Logger LOG = Logger.getLogger(FileTransformer.class.getName());
+    private final List<FilterWriter> filters = new ArrayList<>();
   
-  /**
-   * The subclasses implement this method to define what transformation(s) are
-   * applied when writing characters to the output writer. The visit(File file)
-   * method creates an output file and creates a corresponding writer. It then
-   * calls decorateWithFilters and passes the writer as argument. The method
-   * wraps 0, 1 or more filter writers around the original writer and returns 
-   * the result.
-   * 
-   * @param writer the writer connected to the output file
-   * @return the writer decorated by 0, 1 or more filter writers
-   */
-  public abstract Writer decorateWithFilters(Writer writer);
+    /**
+     * The subclasses implement this method to define what transformation(s) are
+     * applied when writing characters to the output writer. The visit(File file)
+     * method creates an output file and creates a corresponding writer. It then
+     * calls decorateWithFilters and passes the writer as argument. The method
+     * wraps 0, 1 or more filter writers around the original writer and returns 
+     * the result.
+     * 
+     * @param writer the writer connected to the output file
+     * @return the writer decorated by 0, 1 or more filter writers
+     */
+    public abstract Writer decorateWithFilters(Writer writer);
 
-  @Override
-  public void visit(File file) {
-    if (!file.isFile()) {
-      return;
-    }
-    try {
-      Reader reader = new InputStreamReader(new FileInputStream(file), "UTF-8");
-      Writer writer = new OutputStreamWriter(new FileOutputStream(file.getPath()+ ".out"), "UTF-8"); // the bug fix by teacher
-      writer = decorateWithFilters(writer);
+    @Override
+    public void visit(File file) {
+        if (!file.isFile()) {
+            return;
+        }
+        try {
+            Reader reader = new InputStreamReader(new FileInputStream(file), "UTF-8");
+            Writer writer = new OutputStreamWriter(new FileOutputStream(file.getPath()+ ".out"), "UTF-8"); // the bug fix by teacher
+            writer = decorateWithFilters(writer);
 
-      /*
-       * There is a missing piece here: you have an input reader and an ouput writer (notice how the 
-       * writer has been decorated by the concrete subclass!). You need to write a loop to read the
-       * characters and write them to the writer.
-       */
-      
-      reader.close();
-      writer.flush();
-      writer.close();
-    } catch (IOException ex) {
-      LOG.log(Level.SEVERE, null, ex);
+            /*
+             * There is a missing piece here: you have an input reader and an ouput writer (notice how the 
+             * writer has been decorated by the concrete subclass!). You need to write a loop to read the
+             * characters and write them to the writer.
+             */
+            int c = reader.read();
+
+            while (c != -1) {
+
+                writer.write(c);
+                c = reader.read();
+            }
+
+            reader.close();
+             writer.flush();
+            writer.close();
+        } catch (IOException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        }
     }
-  }
 
 }
