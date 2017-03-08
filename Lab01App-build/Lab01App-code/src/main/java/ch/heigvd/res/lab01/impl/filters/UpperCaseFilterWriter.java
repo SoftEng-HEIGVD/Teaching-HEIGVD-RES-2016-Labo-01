cf59016930d6fbcc87fa7c4fd38b03a5f77b6ca3
Gallouche <gallandat.t@gmail.com>
2017-03-08 11:48:16 +0100

    UpperCaseFilterWriterTest passed

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -16,12 +16,14 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+     for(int i = off; i < off + len; i++)
+        write(str.charAt(i));
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+     for(int i = off; i < off + len; i++)
+        write(cbuf[i]);
   }
 
   @Override
