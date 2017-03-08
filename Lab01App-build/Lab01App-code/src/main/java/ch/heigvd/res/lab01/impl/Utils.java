68b0d659acdb0b75dd06c9f8d93270e10a26cea8
Abass Mahdavi <abass.mahdavi@heig-vd.ch>
2017-03-08 23:31:20 +0100

    Utils.java fixed

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Utils.java
@@ -20,7 +20,45 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      String[] result = new String[2];
+      int r = lines.indexOf('\r');
+      int n = lines.indexOf('\n');
+      
+      if (n < 0 & r < 0){
+          result[1] = lines;
+          result[0] = "";
+      } else if (n >= 0 & (n < r | r < 0 )){
+          result[0] = lines.substring(0, n + 1);
+          
+          if (n < lines.length()){
+              result[1] = lines.substring(n + 1);
+          } else {
+            result[1] = "";
+          }
+      } else {    // r > 0 & (r < n | n < 0)
+          if (r == lines.length() - 1){
+              result[0] = lines;
+              result[1] = "";
+          } else if (lines.charAt(r+1) != '\n'){  // r < lines.length()
+              result[0] = lines.substring(0, r + 1);
+              result[1] = lines.substring(r + 1);
+          } else {  //lines.charAt(r+1) == '\n'
+              if (r + 1 == lines.length()){
+                  result[0] = lines;
+                  result[1] = "";
+              } else {
+                  result[0] = lines.substring(0, r + 2);
+                  result[1] = lines.substring(r + 2);
+              }
+          }
+
+      }
+      
+
+      
+      return result;
+     
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
   }
 
 }
