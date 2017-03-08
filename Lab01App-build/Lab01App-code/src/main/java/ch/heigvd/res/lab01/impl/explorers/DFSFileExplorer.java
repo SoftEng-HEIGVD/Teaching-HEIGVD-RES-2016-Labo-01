3806c697d717061e237589be408f27b976395878
P-B Monaco <pierre-benjamin.monaco@heig-vd.ch>
2017-03-08 23:16:24 +0100

    rewriting DFS to match expected output

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,8 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FileWriter;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -14,17 +16,81 @@ import java.io.File;
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
+
+  /*
+    It wasn't easy to implement this part beacause we dont see DFS the same way:
+    I see the file system with file and dir equally, beeing a dir is just a file with
+    specific attributes and a pointer to the next list of file. So, I implemented my
+    DFS taking file and dir as nodes. I finally saw your exemple of DFS :
+
+    ./workspace/quotes
+    ./workspace/quotes/A
+    ./workspace/quotes/A/file1.txt
+    ./workspace/quotes/A/file2.txt
+    ./workspace/quotes/A/B
+    ./workspace/quotes/A/C
+    ./workspace/quotes/A/C/file1.txt
+    ./workspace/quotes/A/C/D
+    ./workspace/quotes/A/C/D/file1.txt
+    ./workspace/quotes/A/C/D/file2.txt
+
+    Mine was :
+
+    .\workspace\quotes
+    .\workspace\quotes\A
+    .\workspace\quotes\A\B
+    .\workspace\quotes\A\C
+    .\workspace\quotes\A\C\D
+    .\workspace\quotes\A\C\D\file1.txt
+    .\workspace\quotes\A\C\D\file2.txt
+    .\workspace\quotes\A\C\file1.txt
+    .\workspace\quotes\A\file1.txt
+    .\workspace\quotes\A\file2.txt
+
+    my old code :
+
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    if(rootDirectory.isFile())
-    {
-      vistor.visit(rootDirectory);
-    }
-    else
+    vistor.visit(rootDirectory);
+    if(rootDirectory.isDirectory())
     {
       for(File fileInDir : rootDirectory.listFiles())
       {
-        explore(fileInDir,vistor);
+        explore(fileInDir, vistor);
+      }
+    }
+  }
+
+
+  As f character from "file1" is higher than B or C the DFS goes into B and C first.
+  It would be kind to specify that you have a kind of priority on file in DFS for the
+  next year students.
+ */
+
+  @Override
+  public void explore(File rootDirectory, IFileVisitor vistor) {
+    vistor.visit(rootDirectory);
+    if(rootDirectory.isDirectory())
+    {
+      //Browse files (with filter)
+      for(File fileInDir : rootDirectory.listFiles(new FileFilter() {
+        @Override
+        public boolean accept(File file) {
+          return file.isFile();
+        }
+      }))
+      {
+        vistor.visit(fileInDir);
+      }
+
+      //Then browse dirs
+      for(File dirInDir : rootDirectory.listFiles(new FileFilter() {
+        @Override
+        public boolean accept(File file) {
+          return file.isDirectory();
+        }
+      }))
+      {
+        explore(dirInDir, vistor);
       }
     }
   }
