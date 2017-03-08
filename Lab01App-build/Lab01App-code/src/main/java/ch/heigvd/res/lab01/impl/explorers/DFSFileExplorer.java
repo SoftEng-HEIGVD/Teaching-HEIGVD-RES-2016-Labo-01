e91df1b2aa45f0eb3c597cec009cb13a30b89350
Naewy <katomey@gmail.com>
2017-03-08 10:16:16 +0100

    DFSFileExplorer.java implemented to pass the 2 tests in DFSFileExplorer.java

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -11,12 +12,30 @@ import java.io.File;
  * files in the directory and then moves into the subdirectories.
  * 
  * @author Olivier Liechti
+ * Modified by Lara Chauffoureaux on 08.03.2017
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
-
+    @Override
+    public void explore(File rootDirectory, IFileVisitor vistor) {
+                
+        // for this method use the same method that i described in the google form
+        // to perform the exploration of the file system
+        vistor.visit(rootDirectory);
+        
+        // Stop condition of the recursion, if we aren't in a directory, we visit
+        // it and return back up directly
+        if(rootDirectory.isDirectory()) {  
+            
+            File[] files = rootDirectory.listFiles();
+            
+            // i can do this because File implemented the interface comparable
+            Arrays.sort(files); 
+            
+            for(File f : files) {
+                
+                explore(f, vistor); // recursive call on all subfiles
+            }
+        }
+    }
 }
