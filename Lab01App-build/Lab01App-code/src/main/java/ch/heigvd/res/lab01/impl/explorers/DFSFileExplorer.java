e19c127fb547c817c5ebd094ae3b1aa797567166
GOthGir <gaetan.othenin-girard@heig-vd.ch>
2017-03-09 14:42:16 +0100

    Application.java and DFSFileExplorer.java tests passed.

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.io.FileFilter;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -14,9 +15,23 @@ import java.io.File;
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+   public void explore(File rootDirectory, IFileVisitor vistor) {
+      vistor.visit(rootDirectory); // The current directory is visited.
 
+      // We explore if the given directory is a directory.
+      if (rootDirectory.isDirectory()) {
+         // We first visit each files from the current directory.
+         for (File file : rootDirectory.listFiles()) {
+            if (file.isFile()) {
+               vistor.visit(file);
+            }
+         }
+
+         // Then we explore each sub directory.
+         for (File file : rootDirectory.listFiles()) {
+            if (file.isDirectory())
+               explore(file, vistor);
+         }
+      }
+   }
 }
