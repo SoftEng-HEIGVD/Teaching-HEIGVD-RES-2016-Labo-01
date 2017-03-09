0f4a97af4019984e76fd20f0f4079e9cdc918383
Camilo PINEDA SERNA <camilo.pinedaserna@heig-vd.ch>
2017-03-09 10:47:02 +0100

    DFS done and passes tests

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -2,6 +2,7 @@ package ch.heigvd.res.lab01.impl.explorers;
 
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
+
 import java.io.File;
 
 /**
@@ -9,14 +10,46 @@ import java.io.File;
  * exploration of the file system and invokes the visitor for every encountered
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
- * 
- * @author Olivier Liechti
+ *
+ * @author Olivier Liechti, Camilo Pineda Serna
  */
-public class DFSFileExplorer implements IFileExplorer {
+public class DFSFileExplorer implements IFileExplorer
+{
+    /**
+     * explores (DFS pre-order) the file system from a specific node
+     * and invokes the visitor.visit() methode to each node (even if it doesn't exist)
+     * @param rootDirectory the directory where to start the traversal
+     * @param vistor defines the operation to be performed on each file
+     */
+    @Override
+    public void explore(File rootDirectory, IFileVisitor vistor)
+    {
+        // there is a test for this :
+        // if current node does not exist
+        // visit anyway and stops iteration
+        if (!rootDirectory.exists())
+        {
+            vistor.visit(rootDirectory);
+            return;
+        }
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+        // visit current node
+        vistor.visit(rootDirectory);
 
+        // recursion
+        // 1) if a file was reached, halt recursion
+        if (rootDirectory.isFile())
+        {
+            return;
+        }
+        else // 2) else, continue recursion
+        {
+            File[] currentFiles = rootDirectory.listFiles();
+            // we must go deeper
+            for (File currentFile : currentFiles)
+            {
+                explore(currentFile, vistor);
+            }
+        }
+    }
 }
