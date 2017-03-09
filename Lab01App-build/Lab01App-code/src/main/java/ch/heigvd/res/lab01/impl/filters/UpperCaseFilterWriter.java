744eeb4e307b7a44846661cc2aa60609b3d1ed8e
ludelafo <ludovic.delafontaine@gmail.com>
2017-03-09 18:56:21 +0000

    Uppercase filter done

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -14,19 +14,41 @@ public class UpperCaseFilterWriter extends FilterWriter {
     super(wrappedWriter);
   }
 
+  /**
+   * This method uppercase a range from a String
+   * 
+   * @author Ludovic Delafontaine
+   * @param str the input String
+   * @param off the beginning to uppercase
+   * @param len the length to uppercase
+   */
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    out.write(new String(str.getBytes(), off, len).toUpperCase());
   }
 
+  /**
+   * This method uppercase a range from an array of characters
+   * 
+   * @author Ludovic Delafontaine
+   * @param cbuf the input characters array
+   * @param off the beginning to uppercase
+   * @param len the length to uppercase
+   */
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    out.write(new String(cbuf, off, len).toUpperCase());
   }
 
+  /**
+   * This method uppercase one character
+   * 
+   * @author Ludovic Delafontaine
+   * @param c the character to uppercase
+   */
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    out.write(Character.toUpperCase(c));
   }
 
 }
