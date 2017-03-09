9674e4e7a1d7d931fc5797c0732de8677571b796
Arnold von Bauer Gauss <arnold.vonbauergauss@heig-vd.ch>
2017-03-09 17:05:35 +0100

    Ecriture dans un fichier

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Application.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/Application.java
@@ -7,12 +7,8 @@ import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import ch.heigvd.res.lab01.quotes.QuoteClient;
 import ch.heigvd.res.lab01.quotes.Quote;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.StringWriter;
-import java.io.Writer;
+
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
@@ -92,6 +88,7 @@ public class Application implements IApplication {
        * one method provided by this class, which is responsible for storing the content of the
        * quote in a text file (and for generating the directories based on the tags).
        */
+      storeQuote(quote, "quotes-" + i + ".utf8");
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
         LOG.info("> " + tag);
@@ -125,7 +122,17 @@ public class Application implements IApplication {
    * @throws IOException 
    */
   void storeQuote(Quote quote, String filename) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String filePath = WORKSPACE_DIRECTORY + "/";
+    for(String tag : quote.getTags())
+      filePath += tag + "/";
+    filePath += filename;
+
+    File f = new File(filePath);
+    f.getParentFile().mkdirs();
+    f.createNewFile();
+
+    PrintWriter writer = new PrintWriter(f, "UTF-8");
+    writer.print(quote.getQuote());
   }
   
   /**
@@ -142,13 +149,18 @@ public class Application implements IApplication {
          * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
          * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
          */
+        try {
+          writer.write(file.getPath());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
       }
     });
   }
   
   @Override
   public String getAuthorEmail() {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    return "arnold.vonbauergauss@heig-vd.ch";
   }
 
   @Override
