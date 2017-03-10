40129bd880ca2cf67149facccc9c81d149084a0a
Benoit Gianinetti <benoit.gianinetti@gmail.com>
2017-03-10 20:25:14 +0100

    Implements DFSFileExplorer.java

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,9 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -15,8 +18,35 @@ import java.io.File;
 public class DFSFileExplorer implements IFileExplorer {
 
   @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+  public void explore(File rootDirectory, IFileVisitor visitor) {
+    File[] directory = rootDirectory.listFiles(new FileFilter() {
+      @Override
+      public boolean accept(File file) {
+        return file.isDirectory();
+      }
+    });
 
+    File[] files = rootDirectory.listFiles(new FileFilter() {
+      @Override
+      public boolean accept(File file) {
+        return file.isFile();
+      }
+    });
+
+    visitor.visit(rootDirectory);
+
+    if (files != null) {
+      Arrays.sort(files);
+
+      for (File f : files)
+            visitor.visit(f);
+    }
+
+    if (directory != null) {
+      Arrays.sort(directory);
+
+      for (File f : directory)
+        explore(f, visitor);
+    }
+  }
 }
