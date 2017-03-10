982fa8b1948096fb9e2502c02dff0b6255a007de
Benoit Gianinetti <benoit.gianinetti@gmail.com>
2017-03-11 00:14:03 +0100

    Implements FileNumberingFilterWriter. All 10 tests passed.

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -11,31 +11,64 @@ import java.util.logging.Logger;
  * It then sends the line number and a tab character, before resuming the write
  * process.
  *
- * Hello\n\World -> 1\Hello\n2\tWorld
+ * Hello\n\World -> 1\tHello\n2\tWorld
  *
  * @author Olivier Liechti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private int lineCounter = 0;
+  private boolean readCarriageReturn = false;
+
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
+  private String getNextNumbering() {
+    return Integer.toString(++lineCounter) + '\t';
+  }
+
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for (int i = 0; i < len; i++)
+      write(str.charAt(off + i));
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for (int i = 0; i < len; i++)
+      write(cbuf[off + i]);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
 
+    if (readCarriageReturn) {
+      // Windobe EOL
+      if ((char) c == '\n') {
+        out.write(c);
+        out.write(getNextNumbering());
+      }
+      // MacOS EOL
+      else {
+        out.write(getNextNumbering());
+        out.write(c);
+      }
+
+    } else {
+      // For the first line of the file
+      if (lineCounter == 0)
+        out.write(getNextNumbering());
+
+      out.write(c);
+
+      // Linux EOL
+      if ((char) c == '\n')
+        out.write(getNextNumbering());
+    }
+
+    readCarriageReturn = (char) c == '\r';
+  }
 }
