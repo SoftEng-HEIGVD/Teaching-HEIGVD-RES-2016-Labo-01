cd94a990f7c32716d62f0d6eddf60e7b18e96b2d
Lou <luana.martelli@heig-vd.ch>
2017-03-10 15:39:17 +0100

    Labo 01 3 files modified All test are green Comments

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -18,26 +19,31 @@ public class DFSFileExplorer implements IFileExplorer {
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
 
-    /* If the direcroy does not exists, we just add it to the list and we are done */
-    if (!rootDirectory.exists()) {
-      vistor.visit(rootDirectory);
-      return;
-    }
     /* Visits the directory first or the root will never be visited */
     vistor.visit(rootDirectory);
 
-    /* Creates an array of the files in the directory */
-    File[] files = rootDirectory.listFiles();
-
-    for (File f : files) {
-      /* If files, we visit */
-      if (f.isFile()) {
-        vistor.visit(f);
-        /* If directory, we recursively call the method */
-      } else {
-        explore(f, vistor);
-      }
+    /* If the direcroy does not exists, we are done */
+    if (!rootDirectory.exists()) {
+      return;
     }
 
-  }
+    /* Creates an array of the files in the directory
+     * and sorts it because with this method, we are not sure
+     * that it will always lists files the same way */
+      File[] files = rootDirectory.listFiles();
+      Arrays.sort(files);
+
+      /* We first check if it'a file and we visit it */
+      for (File f : files) {
+        if (f.isFile()) {
+          vistor.visit(f);
+        }
+      }
+
+      /* Then we check if there is a directory and we eventually explore it */
+      for (File f : files) {
+        if (f.isDirectory())
+          explore(f, vistor);
+      }
+    }
 }
--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/transformers/FileTransformer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/transformers/FileTransformer.java
@@ -24,6 +24,7 @@ import java.util.logging.Logger;
  * a list of filters and decorates the output writer with them.
  * 
  * @author Olivier Liechti
+ * @modified by Luana Martelli
  */
 public abstract class FileTransformer implements IFileVisitor {
 
@@ -53,11 +54,12 @@ public abstract class FileTransformer implements IFileVisitor {
       Writer writer = new OutputStreamWriter(new FileOutputStream(file.getPath()+ ".out"), "UTF-8"); // the bug fix by teacher
       writer = decorateWithFilters(writer);
 
-      /*
-       * There is a missing piece here: you have an input reader and an ouput writer (notice how the 
-       * writer has been decorated by the concrete subclass!). You need to write a loop to read the
-       * characters and write them to the writer.
-       */
+      /* We read the reader until no character found
+       * and we write it in the writer */
+      int character;
+      while ((character = reader.read()) != -1) {
+        writer.write((char)character);
+      }
       
       reader.close();
       writer.flush();
