0b366592ed86464124164822a7147b623e5e0fa2
Nicolas Rod <ncls.rod@gmail.com>
2017-03-11 17:18:22 +0100

    correction in the DFS explorer

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,27 +3,42 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
- * exploration of the file system and invokes the visitor for every encountered
- * node (file and directory). When the explorer reaches a directory, it visits all
- * files in the directory and then moves into the subdirectories.
- * 
+ * exploration of the file system and invokes the visitor for every encountered node
+ * (file and directory). When the explorer reaches a directory, it visits all files
+ * in the directory and then moves into the subdirectories.
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-      File[] files = rootDirectory.listFiles();
-      for (File file : files) {
-          if (file.isDirectory()) {
-              explore(file, vistor);
-          } else {
-              vistor.visit(file);
-          }
-      }
-  }
+    @Override
+    public void explore(File rootDirectory, IFileVisitor vistor) {
 
+        vistor.visit(rootDirectory);
+        
+        if (rootDirectory.isDirectory()){
+            File[] filesAndDirectories = rootDirectory.listFiles();
+            Arrays.sort(filesAndDirectories);
+            
+            if (filesAndDirectories != null){
+                LinkedList<File> directories = new LinkedList<>();
+                
+                for (File f : filesAndDirectories){
+                    if (f.isFile()){
+                        explore(f, vistor);
+                    } else if (f.isDirectory()){
+                        directories.add(f);
+                    }
+                }
+                for (File f : directories){
+                    explore(f, vistor);
+                }
+            }
+        }
+    }
 }
