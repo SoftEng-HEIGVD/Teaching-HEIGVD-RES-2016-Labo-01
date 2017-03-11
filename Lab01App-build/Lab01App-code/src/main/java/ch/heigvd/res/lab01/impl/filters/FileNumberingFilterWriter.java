18c5a912114e9d5c6f4d9ff86d34671e3c9c6cf3
Nicolas Rod <ncls.rod@gmail.com>
2017-03-11 16:44:35 +0100

    FileNumbering passing tests

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/FileNumberingFilterWriter.java
@@ -6,10 +6,9 @@ import java.io.Writer;
 import java.util.logging.Logger;
 
 /**
- * This class transforms the streams of character sent to the decorated writer.
- * When filter encounters a line separator, it sends it to the decorated writer.
- * It then sends the line number and a tab character, before resuming the write
- * process.
+ * This class transforms the streams of character sent to the decorated writer. When
+ * filter encounters a line separator, it sends it to the decorated writer. It then
+ * sends the line number and a tab character, before resuming the write process.
  *
  * Hello\n\World -> 1\Hello\n2\tWorld
  *
@@ -17,25 +16,50 @@ import java.util.logging.Logger;
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private int lineCounter = 1;
+    private int previousChar = -1;
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+    public FileNumberingFilterWriter(Writer out) {
+        super(out);
+    }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        this.write(str.toCharArray(), off, len);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        for (int i = off; i < off + len; i++){
+            this.write(cbuf[i]);
+        }
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(int c) throws IOException {
+        // Check if this is the first line
+        if (lineCounter == 1){
+            out.write(lineCounter + "\t");
+            out.write(c);
+            lineCounter++;
+        } else {
+            // Check for the MACOS separator
+            if (previousChar == '\r' && c != '\n') {
+                out.write(lineCounter + "\t");
+                lineCounter++;
+            }
+            
+            out.write(c);
+            
+            // Check if we have a Unix / Windows new line
+            if (c == '\n'){
+                out.write(lineCounter + "\t");
+                lineCounter++;
+            }
+        }
+        // update previous char to further check if it is the MACOS separator
+        previousChar = c;
+    }
 
 }
