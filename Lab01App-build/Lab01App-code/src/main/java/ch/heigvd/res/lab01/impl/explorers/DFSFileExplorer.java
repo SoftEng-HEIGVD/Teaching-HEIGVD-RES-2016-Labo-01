e5c5f16cdded55c2af74e44949d78b4da6241f92
Basile Châtillon <basile.chatillon@heig-vd.ch>
2017-03-12 14:34:30 +0100

    fix the DFS so it finnaly works

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -14,9 +15,36 @@ import java.io.File;
  */
 public class DFSFileExplorer implements IFileExplorer {
 
+    
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // first we must visit the rootDirectory
+    vistor.visit(rootDirectory);
+    
+    // then we must search all files and subdirectory of the root.
+    // to do that, we must first get them, sort the so they are in an alphabet order
+    // then we visit all the files first
+    // then we finally visit all the sub-directories
+    
+    //cas if the rootDirectory is not a Directory or is empty
+    if (rootDirectory.isDirectory() && rootDirectory.listFiles() != null){
+      //we get all the files and sub-directories
+      File[] files = rootDirectory.listFiles();
+      Arrays.sort(files);
+      
+      // first we explore all the files
+      for(File f : files){
+        if(f.isFile()){
+          explore(f, vistor);
+          }
+      }
+        
+      // then all the directories
+      for(File f : files){
+        if(f.isDirectory()){
+          explore(f, vistor);
+        }
+      }
+    }
   }
-
 }
